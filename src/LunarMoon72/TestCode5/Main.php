c6852720a821f920a22bf49a546ac6f1e8200ddf
LunarMoon72 <88513736+LunarMoon72@users.noreply.github.com>
2021-08-13 15:53:59 -0400

    Update Main.php

--- src/LunarMoon72/TestCode5/Main.php
+++ src/LunarMoon72/TestCode5/Main.php
@@ -3,28 +3,31 @@
 namespace LunarMoon72\TestCode5;
 
 use pocketmine\plugin\PluginBase;
-use pocketmine\event\Listener;
 
 use pocketmine\Player;
-use pocketmine\Server;
-use pocketmine\player\Inventory;
 
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
+use pocketmine\utils\TextFormat;
 
-class TestCode5 extends PluginBase {
-	public function onEnabled(){
-		$this->getServer()->getPluginManager()->registerEvents($this,$this);
-	}
-	public function onCommand(CommandSender $sender, Command $cmd, String $label, Array $args) : bool{
-		switch($cmd->getName()){
-			case "test5":
-			  if($sender instanceof Player){
-			  	$sender->sendMessage("Please use this command ingame!");
-			  }
-			  $sender->getInventory()->addItem(Item::get(1, 0, 1));
-			  $sender->sendMessage("You have been given id 1!");
-			  
-		}
-	}
+class Main extends PluginBase{
+    
+    public function onLoad(){
+    	$this->getLogger()->info("Plugin is loading");
+    }
+    public function onEnabled(){
+    	$this->getLogger()->info("Plugin is Enabled");
+    }
+    public function onCommand(CommandSender $sender, Command $cmd, String $label, Array $args) : bool{
+    	if(!$sender instanceof Player){
+    		$sender->sendMessage("GET INGAME YOU MOTHER FUCKER CUNT NIGGER FUCKER");
+    	}else{
+    		if(!isset($args[0]) or (is_int($args[0]) and $args[0] > 0)){
+    			$args[0] = 4;
+    		}
+    		$sender->getInventory()->addItem(Item::get(1,0,$args[0]));
+    		$sender->sendMessage("YO MOTHER FUCKER YOU GOT GRASS YOU NIGGA");
+    	}
+    	return true;
+    }
 }
